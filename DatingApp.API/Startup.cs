f54de5e7a826deceecbf559b91ef7bec607dd5fc
AmroMbd <37118920+AmroMbd@users.noreply.github.com>
2020-07-10 10:05:06 +0300

    End of Section 8 (Extending the API)

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -19,6 +19,7 @@ using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using DatingApp.API.Helpers;
+using AutoMapper;
 
 namespace DatingApp.API
 {
@@ -38,7 +39,9 @@ namespace DatingApp.API
             services.AddControllers();
           //  services.AddMvc();    //i added 
             services.AddCors();
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository,DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
